a22bd401c109ee124bdc18ae913e80b4cd6c666e
affirnanda <148478207+affirnanda@users.noreply.github.com>
2025-03-03 00:01:26 +0700

    Add files via upload
    arsitektur pak zaki pertemuan 1

---
+++ arsitek1201230001/src/main/java/com/mycompany/arsitek1201230001/firnanda1201230001.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package com.mycompany.arsitek1201230001;
+
+/**
+ *
+ * @author HP
+ */
+public class firnanda1201230001 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
